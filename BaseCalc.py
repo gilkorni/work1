5c1c22af829360dbdc05df98645a195f65155880
DorHab <dorhaba11@gmail.com>
2023-03-24 17:02:27 +0300

    /

---
+++ BaseCalc.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def DecToHex(num):
+    while num>0 :
+        whole = num // 16
+        remainder = num % 16
+        num = whole
+
+
+
+
+
+
